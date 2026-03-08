e7ccf88bdbd5b6e4d64b1d3e9ea0a9a94395cbe6
al3XxBg <elpromaster69.mx@gmail.com>
2026-03-07 20:48:53 -0600

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,20 @@
+nombre_entrante = "Sopa de trufas"
+precio_entrante = "12 euros"
+nombre_principal = "Filete Mignon con salsa de vino tinto"
+precio_principal = "30 euros"
+nombre_postre = "Tiramisú casero"
+precio_postre = "8 euros"
+ 
+print("Menù del dìa - Restaurante Gourmet\n") 
+print("-- entrante --")
+print(nombre_entrante)
+print("Precio:")
+print(precio_entrante)
+print("-- Plato Principal --")
+print(nombre_principal)
+print("Precio:")
+print(precio_principal)
+print("-- Postre --")
+print(nombre_postre)
+print("precio:")
+print(precio_postre)
